38825168c93379e90ddeef855e2e0a93fb7986a2
kings <530675800@qq.com>
2017-07-27 17:29:31 +0800

    整理代码

---
+++ bin/css.v3.js
@@ -0,0 +1,20 @@
+/*
+	imCss
+	-----------------------------------
+	API
+	https://github.com/ektx/imCss
+*/
+
+
+const fs = require('fs');
+const path = require('path');
+
+// 获取指定目录下所有的文件组件
+const dirFiles = require('dirfiles');
+// 保存样式
+const SAVE_CSS_SPACE = {};
+
+
+
+module.exports = css;
+
