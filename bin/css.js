67291c7330cc378611a5534145daeac753f968da
kings <530675800@qq.com>
2017-08-02 18:14:25 +0800

    提交

--- bin/css.js
+++ bin/css.js
@@ -53,45 +53,57 @@ function getImportCss (data, parentFilePath) {
 
 function clearCssData (filePath) {
 
+	console.log('处理:', filePath)
+
 	let cssFilePath = path.resolve( filePath );
 	let fsStat = '';
 
+
 	let doThisCss = () => {
 		let _css = SAVE_CSS_SPACE[cssFilePath];
+		// 源码
 		_css.origin = fs.readFileSync(filePath, 'utf8');
-
+		// 引用
 		_css.import = getImportCss(_css.origin, cssFilePath);
-
-		_css.get = [];
-
+		// 间接引用 css
+		_css.mod = [];
 
 		for (let i = 0,l = _css.import.length; i < l; i++) {
 
 			let imCssResolve = _css.import[i].resolve;
 
-			clearCssData( imCssResolve )
+			clearCssData( imCssResolve ) 
 
-			if (!(imCssResolve in _css.get)) {
-				_css.get.push( imCssResolve )
+			if (!(imCssResolve in _css.mod)) {
+				_css.mod.push( {
+					// 路径
+					path: imCssResolve,
+					// 修改时间
+					mtime: SAVE_CSS_SPACE[imCssResolve].stat.mtime
+				})
+
+				_css.mod = _css.mod.concat( SAVE_CSS_SPACE[imCssResolve].mod )
 			}
 		}
 
+		// 处理后数据
 		_css.data = _css.origin
 				.replace( /(\/\*.+[\n\r])?@import.+;/gi, '')
 				.replace( /[\r\n]{2,}/g, '\r\n' )
 				.replace( /@charset\s['"](utf)-?8['"];?/i, '')
 				.replace( /(\.{2}\/)+/g, '../');
 
-		console.log( _css );
-
 	}
 
+
+	// 读取状态
 	try {
 		fsStat = fs.statSync( filePath );
 	} catch (err) {
 		fsStat = '';
 	}
 
+	// 内存存储
 	if ( cssFilePath in SAVE_CSS_SPACE) {
 		console.log('存在');
 
@@ -99,7 +111,36 @@ function clearCssData (filePath) {
 
 			doThisCss();
 		} else {
-			console.log('不用更新的')
+			console.log(cssFilePath, '自己不用更新的,检查他的模板调用');
+
+			console.log(SAVE_CSS_SPACE[cssFilePath].mod);
+
+			for ( let val of SAVE_CSS_SPACE[cssFilePath].mod) {
+
+				let modStat;
+
+				try {
+					modStat = fs.statSync( val );
+				} catch (err) {
+					console.log(err);
+					return;
+				}
+
+				// 更新 当前文件比内存的新
+				if (modStat.mtime > SAVE_CSS_SPACE[val].stat.mtime) {
+
+
+
+					// 结束遍历,更新文件
+					return;
+				} 
+				// 相同 不用更新
+				else {
+
+				}
+				console.log('*', val )
+
+			}
 		}
 
 	} else {
@@ -121,6 +162,7 @@ function css(entryFile, outFile, callback) {
 
 	clearCssData( entryFile );
 
+	// console.log(SAVE_CSS_SPACE)
 	console.log(Object.keys(SAVE_CSS_SPACE).length)
 
 }
