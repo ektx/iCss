0276f3c5665aa5c1a368be3af0e1704e7a5273d8
kings <530675800@qq.com>
2017-08-07 17:47:00 +0800

    v0.3.0
    可在无版本控制文件下自由工作
    支持入口文件与调用的模板在变化情况下更新输出文件,在输出文件不存在时也可以自动输出,
    优化格式

--- bin/css.js
+++ bin/css.js
@@ -51,51 +51,66 @@ function getImportCss (data, parentFilePath) {
 }
 
 
-function clearCssData (filePath) {
 
-	console.log('处理:', filePath)
+// 遍历模板
+function eachMods (modArr) {
 
-	let fsStat = '';
-	let needUpdateFromMod = false;
+	let maxTime = '';
+
+	// 查看 mod 是否有变化
+	for (let val of modArr) {
+
+		// 在内存中
+		if (val.path in SAVE_CSS_SPACE) {
+
+			let valInRAM = SAVE_CSS_SPACE[val.path];
+
+			// 不存在时间,就是不存在此文件
+			if (!val.mtime) continue;
+
+			if (val.mtime < valInRAM.stat.mtime) {
+				
+				if (valInRAM.stat.mtime > maxTime)
+					maxTime = valInRAM.stat.mtime
+			} 
+			// 相同时
+			// 读取实体文件,再比较一次
+			else {
+
+				let modStat = getFileStat( val.path );
+
+				// 更新 当前文件比内存的新
+				if (+modStat.mtime > +valInRAM.stat.mtime) {
+
+					// 更新内存文件 方便后面调用了此模板的文件不用去读文件
+					valInRAM.stat = modStat;
+
+					doThisCss( val.path )
 
 
-	let doThisCss = (filePath) => {
-		let _css = SAVE_CSS_SPACE[filePath];
-		// 源码
-		_css.origin = fs.readFileSync(filePath, 'utf8');
-		// 引用
-		_css.import = getImportCss(_css.origin, filePath);
-		// 间接引用 css
-		_css.mod = [];
+				}
 
-		for (let i = 0,l = _css.import.length; i < l; i++) {
+				if (modStat.mtime > maxTime)
+					maxTime = modStat.mtime
 
-			let imCssResolve = _css.import[i].resolve;
-
-			clearCssData( imCssResolve ) 
-
-			if (!(imCssResolve in _css.mod)) {
-				_css.mod.push( {
-					// 路径
-					path: imCssResolve,
-					// 修改时间
-					mtime: SAVE_CSS_SPACE[imCssResolve].stat.mtime
-				})
-
-				if (SAVE_CSS_SPACE[imCssResolve].mod)
-					_css.mod = _css.mod.concat( SAVE_CSS_SPACE[imCssResolve].mod )
 			}
 		}
+		// 不在内存
+		else {
+			console.log('内存中不存在此文件:', val.path )
+		}
 
-		// 处理后数据
-		_css.data = _css.origin
-				.replace( /(\/\*.+[\n\r])?@import.+;/gi, '')
-				.replace( /[\r\n]{2,}/g, '\r\n' )
-				.replace( /@charset\s['"](utf)-?8['"];?/i, '')
-				.replace( /(\.{2}\/)+/g, '../');
 
 	}
 
+	return maxTime;
+}
+
+
+// 读取文件信息
+function getFileStat (filePath) {
+	
+	let fsStat
 
 	// 读取自己的状态
 	try {
@@ -104,86 +119,93 @@ function clearCssData (filePath) {
 		fsStat = '';
 	}
 
-	// 内存存储
-	if ( filePath in SAVE_CSS_SPACE) {
-		console.log('存在');
-
-		// 1. 更新自己的模板
-		// 查看 mod 是否有变化
-		for (let val of SAVE_CSS_SPACE[filePath].mod) {
-			console.log('*', val )
-
-			let valInRAM = SAVE_CSS_SPACE[val.path];
-
-			// 不存在时间,就是不存在此文件
-			if (!val.mtime) continue;
+	return fsStat;		
+}
 
 
-			if (val.mtime < valInRAM.stat.mtime) {
-				// 更新 因为自己引用的文件没有内存中的新
-				console.log('U')
-				needUpdateFromMod = true;
-			} 
-			// 相同时
-			// 读取实体文件,再比较一次
-			else {
+/*
+	处理格式
+*/
+function doThisCss (filePath) {
+	let _css = SAVE_CSS_SPACE[filePath];
+	// 源码
+	_css.origin = fs.readFileSync(filePath, 'utf8');
+	// 引用
+	_css.import = getImportCss(_css.origin, filePath);
+	// 间接引用 css
+	_css.mod = [];
 
-				let modStat;
+	for (let i = 0,l = _css.import.length; i < l; i++) {
 
-				try {
-					modStat = fs.statSync( val.path );
-				} catch (err) {
-					console.log(err);
-				}
+		let imCssResolve = _css.import[i].resolve;
 
-				// 更新 当前文件比内存的新
-				if (modStat.mtime > valInRAM.stat.mtime) {
+		clearCssData( imCssResolve ) 
 
-					// 更新 因为文件夹中的文件比内存中的新
-					needUpdateFromMod = true;
+		if (!(imCssResolve in _css.mod)) {
+			_css.mod.push( {
+				// 路径
+				path: imCssResolve,
+				// 修改时间
+				mtime: SAVE_CSS_SPACE[imCssResolve].stat.mtime
+			})
 
-					// 更新内存文件 方便后面调用了此模板的文件不用去读文件
-					valInRAM.stat = modStat;
-
-					doThisCss( val.path )
-
-				} 
-
-			}
-
+			if (SAVE_CSS_SPACE[imCssResolve].mod)
+				_css.mod = _css.mod.concat( SAVE_CSS_SPACE[imCssResolve].mod )
 		}
-
-		if (SAVE_CSS_SPACE[filePath].stat && SAVE_CSS_SPACE[filePath].stat.mtime < fsStat.mtime) {
-
-			doThisCss( filePath );
-		} else {
-			console.log(filePath, '自己不用更新的,检查他的模板调用');
-
-
-			if (needUpdateFromMod) {
-				console.log('这个文件要更新')
-
-
-			} else {
-				console.log('这个文件不要更新')
-			}
-		}
-
-	} else {
-		console.log('不存在,追加内存');
-
-		SAVE_CSS_SPACE[filePath] = {};
-		// 添加文件状态
-		SAVE_CSS_SPACE[filePath].stat = fsStat;
-		SAVE_CSS_SPACE[filePath].resolve = filePath;
-
-		if (fsStat)
-			doThisCss( filePath );
 	}
+
+	// 处理后数据
+	_css.data = _css.origin
+			.replace( /(\/\*.+[\n\r])?@import.+;/gi, '')
+			.replace( /[\r\n]{2,}/g, '\r\n' )
+			.replace( /@charset\s['"](utf)-?8['"];?/i, '')
+			.replace( /(\.{2}\/)+/g, '../');
 
 }
 
 
+/*
+	将数据追加内存
+	@filePath 文件地址
+*/
+function clearCssData (filePath) {
+
+	let fsStat = getFileStat( filePath );
+	let needUpdateFromMod = false;
+
+	// 内存存储
+	if ( filePath in SAVE_CSS_SPACE ) {
+		// 当前文件在内存中的状态及修改时间 小于 硬盘中修改的时间
+		if (SAVE_CSS_SPACE[filePath].mtime < fsStat.mtime) {
+
+			SAVE_CSS_SPACE[filePath].mtime = fsStat.mtime
+
+		} 
+
+	} else {
+
+		// 不存在,追加内存
+		SAVE_CSS_SPACE[filePath] = {};
+
+		// 添加文件状态
+		SAVE_CSS_SPACE[filePath].stat = fsStat;
+		SAVE_CSS_SPACE[filePath].resolve = filePath;
+		
+		// 是否要保存到硬盘(在本身修改或模板修改过要保存到硬盘)
+		if ( !filePath.includes('parts/') )
+			SAVE_CSS_SPACE[filePath].mtime = fsStat.mtime;
+
+		if (fsStat) {
+			doThisCss( filePath );
+		}
+	}
+
+}
+
+/*
+	合并样式
+	--------------------------------------
+*/
 function mergeThisCssFile( entryFile ) {
 	let result = [];
 	let CssInRAM = SAVE_CSS_SPACE[entryFile];
@@ -219,6 +241,7 @@ function mergeThisCssFile( entryFile ) {
 
 }
 
+
 /*
 	@callback 返回一个保存成功与否的状态,成功 true,失败是 false
 */
@@ -226,9 +249,8 @@ function writeFileInner (savePath, saveData, callback) {
 
 	fs.writeFile(savePath, saveData, 'utf8', err => {
 		if (err) {
-			console.log('保存文件时出错!\n'+ err);
 
-			if (callback) callback(false)
+			if (callback) callback(err)
 			return;
 		}
 
@@ -236,7 +258,9 @@ function writeFileInner (savePath, saveData, callback) {
 	})	
 }
 
-
+/*
+	压缩文件
+*/
 function minCss (savePath, data, minCallback) {
 	
 	let result = data.replace(/(\t|\s{2,}|\/\*(.|\r\n|\n)*?\*\/|\;(?=(\n|\r\n|\t)*?\})|\s(?=\{)|\s(?=\())/g, '')
@@ -250,18 +274,98 @@ function minCss (savePath, data, minCallback) {
 }
 
 
-function css(entryFile, outFile, callback, nodeMin = false, minCallback) {
+function css({entryFile, outFile,  min = false, callback}) {
 
 	let cssFilePath = path.resolve( entryFile );
 
 	clearCssData( cssFilePath );
 
-	let mergeData = mergeThisCssFile( cssFilePath );
+	let saveFile = () => {
+		console.log('ready')
+		if (callback && callback.ready) {
+			callback.ready({
+				status: 'ready',
+				msg: '准备输出文件'
+			})
+		}
 
-	writeFileInner(outFile, mergeData, callback)
+		// 查看输出的文件有没有变化或是否存在
+		let mergeData = mergeThisCssFile( cssFilePath );
+		let mergeHead = `@charset 'utf-8';
+/* 
+	im-css
+	------------------------------------
+	(c) 2017 ektx
+	Welcome Use It!
+	
+	API: 
+	https://github.com/ektx/imCss
+*/\r\n`;
 
-	if (nodeMin)
-		minCss(outFile, mergeData, minCallback)
+		writeFileInner(outFile, mergeHead + mergeData, status => {
+			if (callback && callback.out) {
+				callback.out({
+					status: true,
+					msg: status
+				})
+			}
+		})
+
+		if (min)
+			minCss(outFile, mergeData, status => {
+				if (callback && callback.min) {
+					callback.min({
+						status: true,
+						msg: status
+					})
+				}
+			})
+
+		SAVE_CSS_SPACE[cssFilePath].save = false;
+	}
+
+	// 判断源文件与输出文件的关系
+	try {
+		outFileStat = fs.statSync( outFile )
+	} catch (err) {
+		// 输出文件不存在,保存一份
+		saveFile();
+		return;
+	}
+
+
+	// 主要入口文件旧于目前保存的文件
+	if (outFileStat.mtime < SAVE_CSS_SPACE[cssFilePath].mtime) {
+		saveFile();
+		return;
+	} 
+	// 目前保存好的文件新与主要入口文件
+	else {
+		// 最新变动的模板 新于 输出文件要保存
+		if (eachMods( SAVE_CSS_SPACE[cssFilePath].mod ) > outFileStat.mtime) {
+			saveFile();
+			return;
+		}
+	}
+
+
+	if (callback) {
+
+		if (callback.out) {
+			callback.out({
+				status: false,
+				msg: '文件无修改,不用保存到硬盘'
+			})
+		}
+
+		if (min && callback.min) {
+			callback.min({
+				status: false,
+				msg: '文件无修改,不用保存到硬盘'
+			})
+		}
+	}
+
 
 }
 
--- example/index.js
+++ example/index.js
@@ -1,12 +1,40 @@
 
 const css = require('../index');
 
-css('css/demo.css', 'dist/demo.css', (r) => {
-	console.log(r)
-}, true, (min) => {
-	console.log(min)
+css({
+	entryFile: 'css/demo.css',
+	outFile:  'dist/demo2.css',
+	min: true,
+	callback: {
+		ready: r => {
+			console.log(r)
+		},
+
+		out: r => {
+			console.log(r)
+		},
+
+		min: r => {
+			console.log('min', r)
+		}
+	}
+});
+
+css({
+	entryFile: 'css/demo.css',
+	outFile:  'dist/demo.css',
+	callback: {
+		ready: r => {
+			console.log(r)
+		},
+
+		out: r => {
+			console.log(r)
+		},
+
+		min: r => {
+			console.log('min', r)
+		}
+	}
 })
 
-css('css/demo.css', 'dist/demo2.css', null, true, (min) => {
-	console.log(min)
-})
